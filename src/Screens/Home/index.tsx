99222cdb72ad292180255869e9038dc82ee3fd64
tonymatheus <tony.chaves14@gmail.com>
2021-12-31 02:26:17 -0300

    Updates Home screen

---
+++ src/Screens/Home/index.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+import { View, Text } from "react-native";
+import { ButtonAdd } from "../../components/ButtonAdd";
+import { CategorySelect } from "../../components/CategorySelect";
+import { Profile } from "../../components/Profile";
+import { styles } from "./styles";
+
+export function Home() {
+  return (
+    <View>
+      <View style={styles.header}>
+        <Profile />
+        <ButtonAdd />
+      </View>
+      <View>
+        <CategorySelect />
+      </View>
+    </View>
+  );
+}
